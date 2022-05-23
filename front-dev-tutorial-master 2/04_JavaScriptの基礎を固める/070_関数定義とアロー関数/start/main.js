8ecc3c32a43fd28b95b541a7dd8077f330b87944
makoto <ma31kn@gmail.com>
2022-05-23 16:30:54 +0900

    4-70

---
+++ front-dev-tutorial-master 2/04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,18 @@
+// function hello(name = 'Tom') {
+//   console.log('hello ' + name);
+// }
+
+// const hello = function (name = 'Tom') {
+//   console.log('hello ' + name);
+// }
+
+// const hello = (name, age) => {
+//   console.log('hello ' + name + age)
+// }
+
+// hello('Code Mafia', 10);
+// hello('Code Mafia 2', 20);
+
+const arry = [1,2,3,4,5,6];
+
+arry.forEach((value) => console.log(value));
